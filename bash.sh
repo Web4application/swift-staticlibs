1bc88c09d4270c04b5d6168c9de3622595305d5d
Seriki yakub <167559384+Web4application@users.noreply.github.com>
2026-02-04 05:16:15 +0100

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,3 +1,5 @@
+xcodebuild -showBuildSettings | grep " = " > XcodeBuildVariables_Paper.txt
+xcodebuild -showBuildSettings -json > BuildSettings.json
 git branch -m master main
 git fetch origin
 git branch -u origin/main main
